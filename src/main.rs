f86358c6bdaedadaf79f4dc5f092e51cef27819c
ShafiMunshi <shafimunshi111@gmail.com>
2024-04-02 19:33:25 +0600

    implemented traciing

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use axum::{
 };
 
 use tokio::net::TcpListener;
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
+use tracing_appender::rolling;
+use tracing_subscriber::fmt::writer::MakeWriterExt;
 
 mod models;
 mod routes;
@@ -14,6 +18,18 @@ mod utils;
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
+      // save all the trace into a log file
+      let info_file = rolling::daily("./logs", "info").with_max_level(tracing::Level::INFO);
+
+    tracing_subscriber::fmt()
+    .with_target(false) // target source method won't include in the log message
+    .compact()// get a structured minimalist log message
+    // .pretty()
+    // .json()// can output json format
+    // .with_writer(info_file)// save all the trace in the log file
+    // .with_max_level(tracing::Level::TRACE) // set's all the details of tracing
+    // .with_ansi(false)
+    .init(); // initialize the tracing
 
 
     let db_instance = Arc::new(utils::db_instance::unified_db_instance().await);
@@ -21,11 +37,15 @@ async fn main() -> surrealdb::Result<()> {
 
     let app = Router::new()
         .merge(routes::upazila_route::upazila_routes())
-        .layer(Extension(db_instance));
+        .layer(Extension(db_instance))
+        .layer(TraceLayer::new_for_http()
+            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+    );
 
     let listerner = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     // create a server at port 3000 and connect to it
-    dbg!("Started server at port 3000");
+    tracing::debug!("Started server at port 3000");
 
     axum::serve(listerner, app.into_make_service())
         .await
